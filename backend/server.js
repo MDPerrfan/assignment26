4eef3d9fd709460fecd162b428e9e22d08ac4340
Mohammed Parves <mdperrfan@gmail.com>
2025-04-27 22:00:58 +0600

    Updated cors

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ dotenv.config()
 const app = express()
 const allowedOrigins = [
         'http://localhost:5173',
-        'https://eventhandleserver.vercel.app/'
+        'https://eventhandlerserver.onrender.com'
     ]
     // Middleware
 app.use(cors({
