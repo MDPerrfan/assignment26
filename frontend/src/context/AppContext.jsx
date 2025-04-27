afd7399ef6ca9f7b5922b761db7b29e78764a2ae
Mohammed Parves <mdperrfan@gmail.com>
2025-04-27 21:44:37 +0600

    updated

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -6,7 +6,7 @@ export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
     //const backendUrl = "http://localhost:5000"
-    const backendUrl='https://eventhandleserver-9xkilwmvy-mohammed-parves-projects.vercel.app'
+    const backendUrl = 'https://eventhandlerserver.onrender.com'
     const [events, setEvents] = useState([])
     const [loading, setLoading] = useState()
     const [isLoggedin, setIsLoggedin] = useState(false)
